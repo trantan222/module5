8dceae7c41f620e9985830239fe706d79e8a9255
trantan222 <trantan28302@gmail.com>
2023-10-05 19:21:35 +0700

    m5

--- __07_redux/e1_user/src/App.js
+++ __07_redux/e1_user/src/App.js
@@ -2,14 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import List from "./components/List";
-import Delete from "./components/Delete";
 
 function App() {
   return (
       <>
         <Routes>
           <Route path="/" element={<List/>}></Route>
-          <Route path="/delete/:id" element={<Delete/>}></Route>
         </Routes>
 
       </>
