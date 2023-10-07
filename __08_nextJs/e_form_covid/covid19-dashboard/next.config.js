80425db58cd0b65b4a142c579b68fe97db00d7bf
trantan222 <trantan28302@gmail.com>
2023-10-07 16:15:40 +0700

    m5

---
+++ __08_nextJs/e_form_covid/covid19-dashboard/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // server: {
+  //   port: 3001,
+  // },
+}
+
+module.exports = nextConfig
---
+++ __08_nextJs/e_form_covid/covid19-dashboard/pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+        <title>Covid-19 DashBoard</title>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
